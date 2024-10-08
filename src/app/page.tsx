c3aa03ff2207637548d19a05780fb202289161af
eduardocaminha <caminhae@gmail.com>
2024-10-08 19:14:40 -0300

    Corrige estrutura do projeto e remove arquivos duplicados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
 import { AiTextEditor } from '@/components/ai-text-editor'
-import { ThemeProvider } from '@/components/theme-provider'
 
-// Página inicial do aplicativo
 export default function Home() {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <div className="flex flex-col min-h-screen">
-        <main className="flex-grow">
-          <div className="container mx-auto p-8 sm:p-20">
-            <AiTextEditor />
-          </div>
-        </main>
-      </div>
-    </ThemeProvider>
-  );
+    <main className="container mx-auto p-4">
+      <AiTextEditor />
+    </main>
+  )
 }
