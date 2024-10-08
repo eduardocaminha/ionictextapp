7219c849a655a4f78b3314195ab2f5b8b990bdd5
eduardocaminha <caminhae@gmail.com>
2024-10-08 19:09:21 -0300

    Ajusta configuração da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
-import dynamic from 'next/dynamic'
-import { Footer } from "@/components/footer";
+import { AiTextEditor } from '@/components/ai-text-editor'
+import { ThemeProvider } from '@/components/theme-provider'
 
-const ClientSideThemeProvider = dynamic(() => import('@/components/theme-provider').then(mod => mod.ThemeProvider), {
-  ssr: false
-})
-
-const AiTextEditor = dynamic(() => import('@/components/ai-text-editor').then(mod => mod.AiTextEditor), {
-  ssr: false
-})
-
+// Página inicial do aplicativo
 export default function Home() {
   return (
-    <ClientSideThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <div className="flex flex-col min-h-screen">
         <main className="flex-grow">
           <div className="container mx-auto p-8 sm:p-20">
             <AiTextEditor />
           </div>
         </main>
-        <Footer />
       </div>
-    </ClientSideThemeProvider>
+    </ThemeProvider>
   );
 }
